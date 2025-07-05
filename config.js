0c2e9bd803955df86643c9763ec6b59444e5a955
nelsonphikendy24 <phikendyn@gmail.com>
2025-07-05 01:28:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50943160033",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50948336180",
 // add your bot owner number
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
